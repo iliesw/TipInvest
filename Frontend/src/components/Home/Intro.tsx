fae1163c6dbd8f839c14dbb0aa2c38e556dd8ed1
ilify <iliesmraihia@gmail.com>
2025-02-25 17:08:28 +0100

    fixed into images

--- Frontend/src/components/Home/Intro.tsx
+++ Frontend/src/components/Home/Intro.tsx
@@ -5,8 +5,8 @@ const Into = () => {
   return (
     <div className="flex justify-center mb-20">
       <div className="w-[65vw] h-[35vh] bg-gradient-to-t from-[#008e2650] to-white rounded-lg overflow-visible flex relative">
-        <img className="h-[70vh] absolute bottom-0 w-[90%] h-[160%] left-[-25%]" src="/assets/images/right.png" />
-        <img className="h-[70vh] absolute bottom-0 h-[200%] left-[65%] z-0 mask-image-gradient-to-t from-black to-[#00000080]" src="/assets/images/left.png" />
+        <img className="h-[55vh] absolute bottom-0 w-[90%] left-[-25%]" src="/assets/images/right.png" />
+        <img className="h-[70vh] absolute bottom-0  left-[65%] z-0 mask-image-gradient-to-t from-black to-[#00000080]" src="/assets/images/left.png" />
       </div>
     </div>
   );
