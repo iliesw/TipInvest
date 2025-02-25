60ee026d919159963b91c14ed5b040ba3da0b5c8
ilify <iliesmraihia@gmail.com>
2025-02-25 17:03:14 +0100

    all tailwind

--- Frontend/src/components/Home/Intro.tsx
+++ Frontend/src/components/Home/Intro.tsx
@@ -1,51 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import styled from "styled-components";
-
-const IntoDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-bottom:80px;
-  div {
-    width: 65vw;
-    height: 35vh;
-    background: linear-gradient( 0deg ,#008e26 -200%, white);
-    border-radius: 10px;
-    overflow:visible;
-    display:flex;
-    position:relative;
-    
-  }
-
-  img{
-    height:70vh;
-    position:absolute;
-    bottom:0;
-  }
-
-  .right{
-    width:90%;
-    height:170%;
-    left:-25%;
-  }
-  .left{
-    /* width:100%; */
-    height:200%;
-    left:65%;
-    z-index:0;
-    mask-image:linear-gradient(0deg,black,#00000080);
-  }
-`;
 
 const Into = () => {
   return (
-    <IntoDiv>
-      <div>
-
-      <img className="right" src="/assets/images/right.png"/>
-      <img className="left" src="/assets/images/left.png"/>
+    <div className="flex justify-center mb-20">
+      <div className="w-[65vw] h-[35vh] bg-gradient-to-t from-[#008e2650] to-white rounded-lg overflow-visible flex relative">
+        <img className="h-[70vh] absolute bottom-0 w-[90%] h-[160%] left-[-25%]" src="/assets/images/right.png" />
+        <img className="h-[70vh] absolute bottom-0 h-[200%] left-[65%] z-0 mask-image-gradient-to-t from-black to-[#00000080]" src="/assets/images/left.png" />
       </div>
-    </IntoDiv>
+    </div>
   );
 };
 
