af0be15482dcdb020b1bd5d68715a349cdb6c4fa
ilify <iliesmraihia@gmail.com>
2025-02-26 20:45:31 +0100

    Replace logo image with Logo component in Navbar

--- Frontend/src/components/Home/Navbar.tsx
+++ Frontend/src/components/Home/Navbar.tsx
@@ -50,11 +50,7 @@ const Navbar = () =>{
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  alt=""
-                  src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
-                />
+                <Logo />
               </a>
               <button
                 type="button"
