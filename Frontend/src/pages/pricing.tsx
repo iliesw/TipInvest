1ba7387888fb290e596c8b3c8d7d14715792ad53
ilify <iliesmraihia@gmail.com>
2025-03-24 11:27:27 +0100

    refactor(pricing): remove redundant CSS

--- Frontend/src/pages/pricing.tsx
+++ Frontend/src/pages/pricing.tsx
@@ -1,6 +1,4 @@
 import Prices from "@/components/Pricing/Prices";
-import "../../public/assets/css/fonts.css";
-import "../../public/assets/css/globals.css";
 import Navbar from "@/components/Home/Navbar";
 import { Footer } from "@/components/Home/Footer";
 import Brands from "@/components/Home/Brands";
